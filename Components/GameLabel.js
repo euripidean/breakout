e81fd89a8b059377eac8c3d32b99a03a000db98b
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-01-29 09:59:32 -0800

    Finished redefining elements

--- Components/GameLabel.js
+++ Components/GameLabel.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line import/extensions
-// import Sprite from './Sprite.js';
+import Sprite from './Sprite.js';
 
-// class GameLabel extends Sprite {
-//     constructor() {
+class GameLabel extends Sprite {
+  constructor(x, y, color, text, value, font = '32px Helvetica', align = 'left') {
+    super(x, y, color);
+    this.text = text;
+    this.value = value;
+    this.font = font;
+    this.align = align;
+  }
 
-//     }
+  render(ctx) {
+    ctx.font = this.font;
+    ctx.fillStyle = this.color;
+    ctx.fillText(`${this.text}: ${this.value}`, this.x, this.y);
+  }
+}
 
-// }
-
-// export default GameLabel;
+export default GameLabel;
